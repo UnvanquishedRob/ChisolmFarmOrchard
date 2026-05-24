120292fc129b7187d699445434b84597acc5152c
UnvanquishedRob <armstrong.usapatriot@gmail.com>
2026-05-23 21:53:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE = 'orchard-v1';
-const FILES = ['./index.html', './manifest.json'];
+const CACHE = 'orchard-v2';
+const FILES = [
+  '/ChisolmFarmOrchard/',
+  '/ChisolmFarmOrchard/index.html',
+  '/ChisolmFarmOrchard/manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -16,7 +20,7 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('./index.html')
+      caches.match('/ChisolmFarmOrchard/index.html')
     ))
   );
 });
